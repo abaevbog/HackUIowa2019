bf78e780b96bf946fd01af0cc824da6e58edd2b0
Bogdan Abaev <abaevbog@grinnell.edu>
2019-10-12 11:46:32 -0500

    npm install react-bootstrap bootstrap

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    // initialize state here
+    this.state = {}
+    // and bind methods to the current scope
+    //this.method = this.method.bind(this).
+  }
+
+
+
+  // fires up just before loading the component
+  componentDidMount(){
+
+  }
+
+
+
+
+  render() { 
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Button variant="primary">Does bootstrap work </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
