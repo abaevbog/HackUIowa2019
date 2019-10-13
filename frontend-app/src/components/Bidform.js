22079c3fa0900e187e9aed72ff7a0e18543c377d
nameera231 <muhammad@grinnell.edu>
2019-10-13 08:48:58 -0500

    bidform

--- frontend-app/src/components/Bidform.js
+++ frontend-app/src/components/Bidform.js
@@ -18,41 +18,7 @@ class Bidform extends Component{
             <span class="text-muted">Your cart</span>
             <span class="badge badge-secondary badge-pill">3</span>
           </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
-
+          
           <form class="card p-2">
             <div class="input-group">
               <input type="text" class="form-control" placeholder="Promo code"></input>
@@ -63,45 +29,28 @@ class Bidform extends Component{
           </form>
         </div>
         <div class="col-md-8 order-md-1">
-          <h4 class="mb-3">Billing address</h4>
+          <h4 class="mb-3">Bidding form</h4>
           <form class="needs-validation" novalidate="">
             <div class="row">
               <div class="col-md-6 mb-3">
-                <label for="firstName">First name</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required=""></input>
+                <label for="bid">Enter Bid In Dollars</label>
+                <input type="number" class="form-control" id="bid" placeholder="" value="" required=""></input>
                 <div class="invalid-feedback">
-                  Valid first name is required.
+                  Valid bid is required.
                 </div>
               </div>
               <div class="col-md-6 mb-3">
-                <label for="lastName">Last name</label>
-                <input type="text" class="form-control" id="lastName" placeholder="" value="" required=""></input>
+                <label for="lastName">Re-enter and confirm Bid</label>
+                <input type="number" class="form-control" id="lastName" placeholder="" value="" required=""></input>
                 <div class="invalid-feedback">
-                  Valid last name is required.
+                  Valid bid is required.
                 </div>
               </div>
             </div>
 
-            <div class="mb-3">
-              <label for="username">Username</label>
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">@</span>
-                </div>
-                <input type="text" class="form-control" id="username" placeholder="Username" required=""></input>
-                <div class="invalid-feedback" style={{width: "100%",}}>
-                  Your username is required.
-                </div>
-              </div>
-            </div>
+        
 
-            <div class="mb-3">
-              <label for="email">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" id="email" placeholder="you@example.com"></input>
-              <div class="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
+
 
             <div class="mb-3">
               <label for="address">Address</label>
@@ -117,16 +66,7 @@ class Bidform extends Component{
             </div>
 
             <div class="row">
-              <div class="col-md-5 mb-3">
-                <label for="country">Country</label>
-                <select class="custom-select d-block w-100" id="country" required="">
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please select a valid country.
-                </div>
-              </div>
+              
               <div class="col-md-4 mb-3">
                 <label for="state">State</label>
                 <select class="custom-select d-block w-100" id="state" required="">
@@ -206,7 +146,7 @@ class Bidform extends Component{
               </div>
             </div>
             <hr class="mb-4"></hr>
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Place Bid</button>
           </form>
         </div>
       </div>
